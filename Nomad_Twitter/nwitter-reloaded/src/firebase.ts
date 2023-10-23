d82accdfe03184f0116e65f765b008ac34d9b190
KTG <xorb269@naver.com>
2023-10-24 01:13:08 +0900

    1024 집에서 조금함

--- Nomad_Twitter/nwitter-reloaded/src/firebase.ts
+++ Nomad_Twitter/nwitter-reloaded/src/firebase.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDGad5vr4oBjclLQCWabN3sYok0QUrsYk8",
@@ -13,3 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage = getStorage(app);
+
+export const db = getFirestore(app);
